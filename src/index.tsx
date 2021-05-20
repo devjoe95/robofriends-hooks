d750620c03731937c9fc70404cd16cd3b617ea40
Dev Joe <joe.afify95@gmail.com>
2021-05-20 17:14:52 +0200

    Setup WebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.css";
 import App from "./containers/App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "tachyons";
 import { requestRobots, searchRobots } from "./reducers";
 
@@ -19,3 +20,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 serviceWorkerRegistration.register();
+if (process.env.NODE_ENV === "development") {
+  reportWebVitals(console.log);
+}
